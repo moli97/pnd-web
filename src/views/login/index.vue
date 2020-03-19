786d9d65aa52bc63b86aca93d6144c51f2247000
moli <moli@hulai.com>
2020-03-19 17:20:21 +0800

    login bg

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,101 +1,99 @@
 <template>
-  <div class="login-container">
-    <el-form :model="ruleForm2" :rules="rules2"
-             status-icon
-             ref="ruleForm2"
-             label-position="left"
-             label-width="0px"
-             class="demo-ruleForm login-page">
-      <h3 class="title">系统登录</h3>
-      <el-form-item prop="username">
-        <el-input type="text"
-                  v-model="ruleForm2.username"
-                  auto-complete="off"
-                  placeholder="用户名"
-        ></el-input>
-      </el-form-item>
-      <el-form-item prop="password">
-        <el-input type="password"
-                  v-model="ruleForm2.password"
-                  auto-complete="off"
-                  placeholder="密码"
-        ></el-input>
-      </el-form-item>
-      <el-checkbox
-              v-model="checked"
-              class="rememberme"
-      >记住密码</el-checkbox>
-      <el-form-item style="width:100%;">
-        <el-button type="primary" style="width:100%;" @click="handleSubmit" :loading="logining">登录</el-button>
-      </el-form-item>
-    </el-form>
-  </div>
+  <body id="poster">
+    <el-form class="login-container" label-position="left" label-width="0px">
+    <h3 class="login_title">系统登录</h3>
+    <el-form-item>
+      <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="账号"></el-input>
+    </el-form-item>
+
+    <el-form-item>
+      <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="密码"></el-input>
+    </el-form-item>
+
+    <el-form-item style="width: 100%">
+      <el-checkbox v-model="checked" class="rememberme">记住密码</el-checkbox>
+      <el-button type="primary" style="width: 100%;background: #098dff;border: none" v-on:click="login">登&nbsp;录</el-button>
+    </el-form-item>
+  </el-form>
+  </body>
 </template>
+
 
 <script>
     import {login} from '@/apis/token'
     export default {
-        data(){
+        name: "Login",
+        data() {
             return {
-                logining: false,
-                ruleForm2: {
+                loginForm: {
                     username: '',
-                    password: '',
+                    password: ''
                 },
-                rules2: {
-                    username: [{required: true, message: 'please enter your account', trigger: 'blur'}],
-                    password: [{required: true, message: 'enter your password', trigger: 'blur'}]
-                },
-                checked: false
+                checked: false,
             }
         },
         methods: {
-            // eslint-disable-next-line no-unused-vars
-            handleSubmit(event){
-                this.$refs.ruleForm2.validate((valid) => {
-                    if(valid){
-                        this.logining = true;
-                        login(this.ruleForm2).then(res=>{
-                            localStorage.setItem("accessToken",res.data);
-                            this.logining = false;
-                            this.$router.push({path: '/'});
-                        }).catch(() => {
-                            this.logining = false;
-                            this.$alert('用户名或密码错误', '', {
-                                confirmButtonText: 'ok'
-                            })
-                        })
-                    }else{
-                        return false;
+            login() {
+                login(this.loginForm).then(res=>{
+                    localStorage.setItem("accessToken",res.data);
+                    localStorage.setItem("username",this.loginForm.username);
+                    localStorage.setItem("checked",this.checked);
+                    if (this.checked){
+                        localStorage.setItem("password",this.loginForm.password);
+                    } else {
+                        localStorage.setItem("password",'');
                     }
+                    this.$router.push({path: '/'});
+                }).catch(() => {
+                    this.loginForm.password = '';
+                    this.$alert('用户名或密码错误', '', {
+                        confirmButtonText: 'ok'
+                    })
                 })
             }
         },
-        created() {
+        mounted() {
+            this.checked = localStorage.getItem("checked") == 'true';
+            this.loginForm.username = localStorage.getItem("username");
+            if (this.checked){
+                this.loginForm.password = localStorage.getItem("password");
+            }
             if (localStorage.getItem("accessToken")) {
                 this.$router.push({path: '/'})
             }
         }
-    };
-</script>
+    }
+ </script>
 
-<style scoped>
-  .login-container {
-    width: 100%;
+<style>
+  #poster {
+    background:url("../../assets/nightscape2.jpg") no-repeat;
+    background-position: center;
     height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
   }
-  .login-page {
-    -webkit-border-radius: 5px;
-    border-radius: 5px;
-    margin: 180px auto;
+  body{
+    margin: 0px;
+    padding: 0;
+  }
+
+  .login-container {
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 90px auto;
     width: 350px;
-    padding: 35px 35px 15px;
-    background: #fff;
-    border: 1px solid #eaeaea;
+    padding: 35px 35px 15px 35px;
+    /*background: #fff;*/
+    /*border: 1px solid #eaeaea;*/
     box-shadow: 0 0 25px #cac6c6;
+    /*opacity:0.5;*/
   }
-  label.el-checkbox.rememberme {
-    margin: 0px 0px 15px;
-    text-align: left;
+
+  .login_title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #f3fff2;
   }
 </style>
