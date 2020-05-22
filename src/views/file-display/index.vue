1122cf3b1650476d2e34d268441e6d01c257dd8f
moli <moli@hulai.com>
2020-05-22 10:45:04 +0800

    file display

--- src/views/file-display/index.vue
+++ src/views/file-display/index.vue
@@ -1,25 +1,46 @@
 <template>
   <el-card class="box-card">
-    <el-link type="primary" icon="el-icon-folder" @click="init">根目录</el-link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <el-link type="primary" v-if="requestPath != '/'" @click="parentDirectory">上级目录</el-link>
+    <el-link type="primary" icon="el-icon-folder-opened" @click="init">根目录</el-link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <el-link type="primary" icon="el-icon-folder-opened" v-if="requestPath != '/'" @click="parentDirectory">上级目录</el-link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <el-link type="primary" icon="el-icon-picture-outline-round" @click="onPreview" v-if="images.length>0"></el-link>
+
     <ul>
       <li v-for="(item,index) in fileList" :key="index">
-        <el-link type="primary" icon="el-icon-files" v-if="item.displayType == 'DIRECTORY'" @click="getFileList(item.path)">{{ item.name }}</el-link>
-        <el-link type="primary" :href="item.url" target="_blank" v-if="item.displayType == 'FILE'">{{ item.name }}</el-link>
+        <el-link type="primary"
+                 :icon="getIcon(item)"
+                 v-if="item.displayType == 'DIRECTORY'"
+                 @click="getFileList(item.path)">
+          {{ item.name }}
+        </el-link>
+        <el-link type="primary"
+                 :href="item.url"
+                 target="_blank"
+                 :icon="getIcon(item)"
+                 v-if="item.displayType == 'FILE'">
+          {{ item.name }}
+        </el-link>
       </li>
     </ul>
+    <el-image-viewer
+            v-if="showViewer"
+            :on-close="closeViewer"
+            :url-list="images"/>
   </el-card>
 </template>
 
 <script>
     import {displayFile} from '@/apis/file'
+    import ElImageViewer from "element-ui/packages/image/src/image-viewer";
 
     export default {
         name: "fileDisplay",
+        components: {ElImageViewer},
         data() {
             return {
                 fileList: [],
-                requestPath: "/"
+                requestPath: "/",
+                images: [],
+                showViewer: false,
             }
         },
         methods: {
@@ -31,6 +52,7 @@
                 displayFile({"path": this.requestPath}).then(res => {
                     if (res.msg == "success") {
                         this.fileList = res.data;
+                        this.filterImage();
                     }
                 })
             },
@@ -42,6 +64,29 @@
                 }
                 this.getFileList(this.requestPath.substring(0, number));
             },
+            onPreview() {
+                this.showViewer = this.images.length > 0;
+            },
+            closeViewer() {
+                this.showViewer = false
+            },
+            filterImage() {
+                this.images = [];
+                this.fileList.forEach(f => {
+                    if (f.fileType == "PICTURE") {
+                        this.images.push(f.url)
+                    }
+                });
+            },
+            getIcon(item) {
+                switch (item.fileType) {
+                   case "PICTURE":
+                        return "el-icon-picture-outline-round";
+                    case "DEFAULT":
+                        return "el-icon-folder";
+                }
+                return "el-icon-files";
+            }
         },
         mounted() {
             this.init();
